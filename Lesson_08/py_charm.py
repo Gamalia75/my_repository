99e416d4e8e124bc8a00e281e414ea9e0688e678
Mykhailo Gamalia <mykhailo.gamalia@gmail.com>
2023-04-15 17:49:22 +0300

    Lesson_08

---
+++ Lesson_08/py_charm.py
@@ -0,0 +1,11 @@
+def mane_function():
+    print("First commit")
+    print("Second commit")
+    print("Third commit")
+    print("ABCD")
+    print("BCDE")
+
+
+def test_function():
+    print("test - 1st commit")
+    print("test - 2nd commit")
