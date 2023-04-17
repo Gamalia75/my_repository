6cc5f57f945d911cf7a13b01f4a3f9cb3c722c64
Mykhailo Gamalia <mykhailo.gamalia@gmail.com>
2023-04-17 11:32:08 +0300

    Final project

--- Lesson_08/mane.py
+++
@@ -1,9 +0,0 @@
-def mane_function():
-    print('Hello there!')
-    print('Nice to meet you!')
-    print('Have a nice day!')
-    print('How are you?')
-
-def test_function():
-    print('New code of new function!')
-    print('Another code')
--- Lesson_08/py_charm.py
+++
@@ -1,9 +0,0 @@
-def mane_function():
-    print('Hello there!')
-    print('Nice to meet you!')
-    print('Have a nice day!')
-    print('Have a nice day!')
-
-def test_function():
-    print('New code of new function!')
-    print('Another code')
